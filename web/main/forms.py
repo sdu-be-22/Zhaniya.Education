d8e44148d897baf3b775b5fecd6c6ed7d15675e9
Yernar Madiyar <91130537+irinaqqq@users.noreply.github.com>
2022-04-12 02:59:31 +0600

    Add files via upload

---
+++ web/main/forms.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import Task
+from django.forms import ModelForm, TextInput, Textarea
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "task"]
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите название'
+            }),
+            "task": Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите описание'
+            }),
+        }
