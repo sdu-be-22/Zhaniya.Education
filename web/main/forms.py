ae32f6cba11e48857f7a3349d1108c95192ca020
Yernar Madiyar <samataskarbaiev@gmail.com>
2022-05-06 16:12:35 +0600

    nor

--- web/main/forms.py
+++ web/main/forms.py
@@ -4,12 +4,27 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Task
 from django.forms import ModelForm, TextInput, Textarea
 
+
 class NewUserForm(UserCreationForm):
-	email = forms.EmailField(required=True)
+	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+	first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+	last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+	
 
 	class Meta:
 		model = User
-		fields = ("username", "first_name", "last_name", "email", "password1", "password2")
+		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+	def __init__(self, *args, **kwargs):
+		super(NewUserForm, self).__init__(*args, **kwargs)
+
+		self.fields['username'].widget.attrs['class'] = 'form-control'
+		self.fields['password1'].widget.attrs['class'] = 'form-control'
+		self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+
+
 
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
